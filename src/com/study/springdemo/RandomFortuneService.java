622a646511e9d9517c73547c82b260452e50dd96
YifengZhong <yf_zhong@hotmail.com>
2019-12-04 23:00:50 -0600

    implement RandomFortuneService

--- src/com/study/springdemo/RandomFortuneService.java
+++ src/com/study/springdemo/RandomFortuneService.java
@@ -1,14 +1,23 @@
 package com.study.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
-
+    //create an array strings
+	private String[] data = {
+			"fortune1",
+			"fortune2",
+			"fortune3"
+	};
+	private Random myRandom = new Random();
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		int index = myRandom.nextInt(data.length);
+		String theFortune = data[index];
+		return theFortune;
 	}
 
 }
