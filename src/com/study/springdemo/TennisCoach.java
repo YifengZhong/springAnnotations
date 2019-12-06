27a033c8978a2a9a6502c1078153e7579def4c49
YifengZhong <yf_zhong@hotmail.com>
2019-12-05 22:57:48 -0600

    add life cycle methoe for bean

--- src/com/study/springdemo/TennisCoach.java
+++ src/com/study/springdemo/TennisCoach.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component  //if do not set "thatSillCoach", it will use tennisCoach as default bean ID"
-@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Value("${foo.email}")
@@ -19,8 +18,21 @@ public class TennisCoach implements Coach {
 	@Autowired
 	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
-	
-	
+	//due to java version greater than 9
+	//we should import relative lib manually to
+	//use these anotation.
+	//define my init method
+	/*
+	 * @PostConstruct 
+	 * public void doMyStartupStuff() {
+	 * System.out.println("This ini method."); 
+	 * } //define my destroy method
+	 * 
+	 * @PreDestory 
+	 * public void doMyCleanupStuff() {
+	 * System.out.println("this destroy method");
+	 *  }
+	 */	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach:inside default constructor");
