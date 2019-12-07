266704ddf9a7972c69300f2e066516095241f1de
YifengZhong <yf_zhong@hotmail.com>
2019-12-07 10:06:02 -0600

    Define Spring Beans with Javacode(no xml)

---
+++ src/com/study/springdemo/SadFortuneService.java
@@ -0,0 +1,11 @@
+package com.study.springdemo;
+
+public class SadFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return "Todayis a sad day";
+	}
+
+}
--- src/com/study/springdemo/SportConfig.java
+++ src/com/study/springdemo/SportConfig.java
@@ -1,10 +1,21 @@
 package com.study.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.study.springdemo")
+//@ComponentScan("com.study.springdemo")
 public class SportConfig {
-
+	//define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	//define bean for our swim coach And in ject dependency
+	@Bean
+	public SwimCoach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+	
 }
