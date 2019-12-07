243567d8a6780c0cad17ea27786f75d40e973bd4
YifengZhong <yf_zhong@hotmail.com>
2019-12-07 10:42:35 -0600

    Inject values from Properties File

--- src/com/study/springdemo/SwimCoach.java
+++ src/com/study/springdemo/SwimCoach.java
@@ -1,9 +1,16 @@
 package com.study.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	
 	private FortuneService fortuneService;
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService= theFortuneService;
@@ -19,7 +26,13 @@ public class SwimCoach implements Coach {
 		
 		return fortuneService.getFortune();
 	}
+	public String getEmail() {
+		return email;
+	}
+	public String getTeam() {
+		return team;
+	}
 
-
+	
 
 }
